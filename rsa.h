b1832a3d8d70cd4621ba7e3ec825e92f92bd28af
gooddids22 <splendidotamiri@gmail.com>
2023-06-29 03:52:44 -0700

    factors_functions.c

--- rsa.h
+++ rsa.h
@@ -21,7 +21,8 @@ unsigned short int _atoi(char *src, unsigned short int *dest);
 void _putint(unsigned short int *dest, unsigned short int n);
 
 /* pollard-rho.c */
-unsigned short int modular_pow(unsigned short int base, int exponent, unsigned short int modulus);
+unsigned short int modular_pow(unsigned short int base, int exponent);
+unsigned short int modulus(unsigned short int modulus);
 unsigned short int __gcd(unsigned short int a, unsigned short int b);
 unsigned short int PollardRho(unsigned short int n);
 unsigned short int compute(unsigned short int *src, unsigned short int n);
